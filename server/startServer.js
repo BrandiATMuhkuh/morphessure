d38cc24b1676b2b25f234770f6fdcbc909426b38
Juergen Brandstetter <juergen@tagaprice.org>
2014-05-08 22:44:14 +1200

    added methode for config.json and fetch qualtrics data

--- server/startServer.js
+++ server/startServer.js
@@ -1,15 +1,8 @@
-var Script = require('deployd/lib/script');
+var http = require('http');
 var deployd = require('deployd');
+var config = require('./config.json');
 
-var dpd = deployd({
-  port: 2403,
-  env: 'development',
-  db: {
-    host: '127.0.0.1',
-    port: 12345,
-    name: '-deployd'
-  }
-});
+var dpd = deployd(config.deployd);
 
 
 dpd.listen();
@@ -18,8 +11,43 @@ dpd.sockets.on('connection', function (socket) {
   socket.emit('master:info', { hello: 'world' });
   socket.on('master:start', function (data) {
     console.log(data);
-    socket.emit('master:info', { answer: 'yourData'+data.username });
+    createUser("test");
+    //socket.emit('master:info', { answer: 'yourData'+data.username });
   });
 });
 
-//console.log(dpd.sockets);
+
+/**
+ * adds a perticipant into the database
+ * @param  {[type]} userId Is the id the user gave itself while running the qualtrics question
+ * @return {[type]}        [description]
+ */
+function createUser(userId){
+	console.log("createUser");
+
+	var _pars = new Array(
+		"API_SELECT=ControlPanel&Version=2.3",
+		"Request=getLegacyResponseData",
+		"User="+config.qualtrics.User,
+		"Token="+config.qualtrics.Token,
+		"Format=JSON",
+		"SurveyID="+config.qualtrics.SurveyID,
+		"Labels=0");
+
+	http.get("http://canterbury.qualtrics.com/WRAPI/ControlPanel/api.php?"+_pars.join("&"), function(res) {
+	  console.log("Got response: " + res.statusCode);
+	  var str = '';
+	  res.on("data", function(chunk) {
+	    str += chunk;
+	  });
+
+	  res.on("end", function() {
+	    console.log("end: " + str);
+	    var obj = JSON.parse( str );
+	    console.log(obj);
+	  });
+
+	}).on('error', function(e) {
+	  console.log("Got error: " + e.message);
+	});
+}
