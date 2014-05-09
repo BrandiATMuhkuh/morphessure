d79923ab259482b628a384d19bc52a2e271d1be6
Juergen Brandstetter <juergen@tagaprice.org>
2014-05-10 02:02:47 +1200

    read practice file from database instead of file

--- server/public/mapPlayer0.js
+++ server/public/mapPlayer0.js
@@ -1,218 +1,178 @@
-_mp[0] = {
-    properties: {
-        rowSize: 6,
-        next: 3,
-        dbId: null,
-        experimenter: true
-    },
-    map: [
-        {
-            pos: 0,
-            file: "base_pack/Enemies/fishSwim1.png",
-            trap: true
-        },
-        {
-            pos: 1,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 2,
-            file: "base_pack/HUD/hud_0.png",
-            trap: true,
-            next: 8, 
-            nophemism: "Zero",
-            dysphemism: "",
-            euphemism: ""
-        },
-        {
-            pos: 3,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 4,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 5,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 6,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 7,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 8,
-            file: "base_pack/HUD/hud_1.png",
-            trap: false,
-            next: 14, 
-            nophemism: "One",
-            dysphemism: "",
-            euphemism: ""
-        },
-        {
-            pos: 9,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 10,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos:11,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 12,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 13,
-            file: "base_pack/HUD/hud_2.png",
-            trap: false,
-            next: 18, 
-            nophemism: "Two",
-            dysphemism: "",
-            euphemism: ""
-        },
-        {
-            pos: 14,
-            file: "base_pack/HUD/hud_3.png",
-            trap: false,
-            next: 13, 
-            nophemism: "Three",
-            dysphemism: "",
-            euphemism: ""
-        },
-        {
-            pos: 15,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 16,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos:17,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 18,
-            file: "base_pack/HUD/hud_4.png",
-            trap: false,
-            next: 24, 
-            nophemism: "Four",
-            dysphemism: "",
-            euphemism: ""
-        },
-        {
-            pos: 19,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 20,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 21,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 22,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos:23,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 24,
-            file: "base_pack/HUD/hud_5.png",
-            trap: false,
-            next: 30, 
-            nophemism: "Five",
-            dysphemism: "",
-            euphemism: ""
-        },
-        {
-            pos: 25,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 26,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 27,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 28,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos:29,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 30,
-            file: "base_pack/HUD/hud_6.png",
-            trap: false, 
-            nophemism: "Six",
-            dysphemism: "",
-            euphemism: ""
-        },
-        {
-            pos: 31,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 32,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 33,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 34,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos:35,
-            file: "phaser_50x50.png",
-            trap: true
-        }
-        
-    ]
+_mp[0] = { "map" : [ { "file" : "base_pack/Enemies/fishSwim1.png",
+        "pos" : 0,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 1,
+        "trap" : true
+      },
+      { "dysphemism" : "",
+        "euphemism" : "",
+        "file" : "base_pack/HUD/hud_0.png",
+        "next" : 8,
+        "nophemism" : "Zero",
+        "pos" : 2,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 3,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 4,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 5,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 6,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 7,
+        "trap" : true
+      },
+      { "dysphemism" : "",
+        "euphemism" : "",
+        "file" : "base_pack/HUD/hud_1.png",
+        "next" : 14,
+        "nophemism" : "One",
+        "pos" : 8,
+        "trap" : false
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 9,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 10,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 11,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 12,
+        "trap" : true
+      },
+      { "dysphemism" : "",
+        "euphemism" : "",
+        "file" : "base_pack/HUD/hud_2.png",
+        "next" : 18,
+        "nophemism" : "Two",
+        "pos" : 13,
+        "trap" : false
+      },
+      { "dysphemism" : "",
+        "euphemism" : "",
+        "file" : "base_pack/HUD/hud_3.png",
+        "next" : 13,
+        "nophemism" : "Three",
+        "pos" : 14,
+        "trap" : false
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 15,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 16,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 17,
+        "trap" : true
+      },
+      { "dysphemism" : "",
+        "euphemism" : "",
+        "file" : "base_pack/HUD/hud_4.png",
+        "next" : 24,
+        "nophemism" : "Four",
+        "pos" : 18,
+        "trap" : false
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 19,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 20,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 21,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 22,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 23,
+        "trap" : true
+      },
+      { "dysphemism" : "",
+        "euphemism" : "",
+        "file" : "base_pack/HUD/hud_5.png",
+        "next" : 30,
+        "nophemism" : "Five",
+        "pos" : 24,
+        "trap" : false
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 25,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 26,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 27,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 28,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 29,
+        "trap" : true
+      },
+      { "dysphemism" : "",
+        "euphemism" : "",
+        "file" : "base_pack/HUD/hud_6.png",
+        "nophemism" : "Six",
+        "pos" : 30,
+        "trap" : false
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 31,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 32,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 33,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 34,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 35,
+        "trap" : true
+      }
+    ],
+  "properties" : { "dbId" : null,
+      "experimenter" : true,
+      "next" : 3,
+      "rowSize" : 6
+    }
 };
--- server/public/mapPlayer1.js
+++ server/public/mapPlayer1.js
@@ -1,218 +1,178 @@
-_mp[1] = {
-    properties: {
-        rowSize: 6,
-        next: 3,
-        dbId: null,
-        experimenter: false
-    },
-    map: [
-        {
-            pos: 0,
-            file: "base_pack/Enemies/flyDead.png",
-            trap: true
-        },
-        {
-            pos: 1,
-            file: "base_pack/Items/bomb.png",
-            trap: true
-        },
-        {
-            pos: 2,
-            file: "base_pack/Enemies/slimeWalk1.png",
-            trap: true,
-            next: 8, 
-            nophemism: "Mad Slimy",
-            dysphemism: "",
-            euphemism: ""
-        },
-        {
-            pos: 3,
-            file: "base_pack/Enemies/snailWalk1.png",
-            trap: true
-        },
-        {
-            pos: 4,
-            file: "base_pack/Enemies/blockerSad.png",
-            trap: true
-        },
-        {
-            pos: 5,
-            file: "base_pack/HUD/hud_gem_blue.png",
-            trap: true
-        },
-        {
-            pos: 6,
-            file: "base_pack/HUD/hud_keyBlue.png",
-            trap: true
-        },
-        {
-            pos: 7,
-            file: "base_pack/HUD/hud_keyRed_disabled.png",
-            trap: true
-        },
-        {
-            pos: 8,
-            file: "base_pack/Items/bush.png",
-            trap: false,
-            next: 14,
-            nophemism: "Green Cloud",
-            dysphemism: "",
-            euphemism: ""
-        },
-        {
-            pos: 9,
-            file: "base_pack/HUD/hud_keyRed_disabled.png",
-            trap: true
-        },
-        {
-            pos: 10,
-            file: "base_pack/HUD/hud_coins.png",
-            trap: true
-        },
-        {
-            pos:11,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 12,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 13,
-            file: "base_pack/Items/coinBronze.png",
-            trap: false,
-            next: 18, 
-            nophemism: "Coin",
-            dysphemism: "",
-            euphemism: ""
-        },
-        {
-            pos: 14,
-            file: "base_pack/Items/flagBlue.png",
-            trap: false,
-            next: 13, 
-            nophemism: "Blue Flag",
-            dysphemism: "",
-            euphemism: ""
-        },
-        {
-            pos: 15,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 16,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos:17,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 18,
-            file: "base_pack/Items/cactus.png",
-            trap: false,
-            next: 24, 
-            nophemism: "Kaktus",
-            dysphemism: "",
-            euphemism: ""
-        },
-        {
-            pos: 19,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 20,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 21,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 22,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos:23,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 24,
-            file: "base_pack/Items/chain.png",
-            trap: false,
-            next: 30, 
-            nophemism: "Chain",
-            dysphemism: "",
-            euphemism: ""
-        },
-        {
-            pos: 25,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 26,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 27,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 28,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos:29,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 30,
-            file: "base_pack/Items/gemRed.png",
-            trap: false, 
-            nophemism: "Diamond",
-            dysphemism: "",
-            euphemism: ""
-        },
-        {
-            pos: 31,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 32,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 33,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos: 34,
-            file: "phaser_50x50.png",
-            trap: true
-        },
-        {
-            pos:35,
-            file: "phaser_50x50.png",
-            trap: true
-        }
-        
-    ]
+_mp[1] = { "map" : [ { "file" : "base_pack/Enemies/flyDead.png",
+        "pos" : 0,
+        "trap" : true
+      },
+      { "file" : "base_pack/Items/bomb.png",
+        "pos" : 1,
+        "trap" : true
+      },
+      { "dysphemism" : "",
+        "euphemism" : "",
+        "file" : "base_pack/Enemies/slimeWalk1.png",
+        "next" : 8,
+        "nophemism" : "Mad Slimy",
+        "pos" : 2,
+        "trap" : true
+      },
+      { "file" : "base_pack/Enemies/snailWalk1.png",
+        "pos" : 3,
+        "trap" : true
+      },
+      { "file" : "base_pack/Enemies/blockerSad.png",
+        "pos" : 4,
+        "trap" : true
+      },
+      { "file" : "base_pack/HUD/hud_gem_blue.png",
+        "pos" : 5,
+        "trap" : true
+      },
+      { "file" : "base_pack/HUD/hud_keyBlue.png",
+        "pos" : 6,
+        "trap" : true
+      },
+      { "file" : "base_pack/HUD/hud_keyRed_disabled.png",
+        "pos" : 7,
+        "trap" : true
+      },
+      { "dysphemism" : "",
+        "euphemism" : "",
+        "file" : "base_pack/Items/bush.png",
+        "next" : 14,
+        "nophemism" : "Green Cloud",
+        "pos" : 8,
+        "trap" : false
+      },
+      { "file" : "base_pack/HUD/hud_keyRed_disabled.png",
+        "pos" : 9,
+        "trap" : true
+      },
+      { "file" : "base_pack/HUD/hud_coins.png",
+        "pos" : 10,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 11,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 12,
+        "trap" : true
+      },
+      { "dysphemism" : "",
+        "euphemism" : "",
+        "file" : "base_pack/Items/coinBronze.png",
+        "next" : 18,
+        "nophemism" : "Coin",
+        "pos" : 13,
+        "trap" : false
+      },
+      { "dysphemism" : "",
+        "euphemism" : "",
+        "file" : "base_pack/Items/flagBlue.png",
+        "next" : 13,
+        "nophemism" : "Blue Flag",
+        "pos" : 14,
+        "trap" : false
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 15,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 16,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 17,
+        "trap" : true
+      },
+      { "dysphemism" : "",
+        "euphemism" : "",
+        "file" : "base_pack/Items/cactus.png",
+        "next" : 24,
+        "nophemism" : "Kaktus",
+        "pos" : 18,
+        "trap" : false
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 19,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 20,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 21,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 22,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 23,
+        "trap" : true
+      },
+      { "dysphemism" : "",
+        "euphemism" : "",
+        "file" : "base_pack/Items/chain.png",
+        "next" : 30,
+        "nophemism" : "Chain",
+        "pos" : 24,
+        "trap" : false
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 25,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 26,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 27,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 28,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 29,
+        "trap" : true
+      },
+      { "dysphemism" : "",
+        "euphemism" : "",
+        "file" : "base_pack/Items/gemRed.png",
+        "nophemism" : "Diamond",
+        "pos" : 30,
+        "trap" : false
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 31,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 32,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 33,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 34,
+        "trap" : true
+      },
+      { "file" : "phaser_50x50.png",
+        "pos" : 35,
+        "trap" : true
+      }
+    ],
+  "properties" : { "dbId" : null,
+      "experimenter" : false,
+      "next" : 3,
+      "rowSize" : 6
+    }
 };
