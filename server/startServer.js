54db8d2bc6dad900f247bedceb2b1813cf69076e
Juergen Brandstetter <juergen@tagaprice.org>
2014-05-16 20:54:07 +1200

    changed from css ids to classes

--- server/startServer.js
+++ server/startServer.js
@@ -195,12 +195,14 @@ function getSceneAndReplaceWords(userId, sceneName, qualtricData, words){
 		//Start both clients to do the right nights
 		//console.log(result2, result2.id);
 		console.log("tell client 0 to reload");
+		/*
 		globalSock.emit('client:start', { "client": 0, "levelname":"practice" });
 		
 		setTimeout(function(){
 			console.log("tell client 1 to reload");
 			globalSock.emit('client:start', { "client": 1, "levelname":"practice" });
 		}, 3000);
+		*/
 	});
 
 	});
