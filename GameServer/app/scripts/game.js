cbb4eb4c05adb6d2dcb016b701952a0700fef67b
BrandiATMuhkuh <juergen@tagaprice.org>
2015-03-24 21:31:03 +1300

    changed games.js from es5 to es6

--- GameServer/app/scripts/game.js
+++ GameServer/app/scripts/game.js
@@ -3,14 +3,18 @@
  */
 'use strict';
 
-function Game(phaser) {
-  this.phaser = phaser;
-};
+class Game {
 
-Game.prototype = {
-  preload: function() {
+  constructor(phaser){
+    this.phaser = phaser;
+    this.cursors = null;
+    this.tileArray = null;
+  }
+
+  preload(){
     // load preloader assets
 
+    //This array contains all background prefabs
     this.tileArray = [
       'tileAutumn.png',
       'tileAutumn_full.png',
@@ -34,31 +38,29 @@ Game.prototype = {
       'tileWater_full.png',
       'tileWater_shadow.png'];
 
+
+    //Load all images int the cache
     for(var tile in this.tileArray){
-      console.log(this.tileArray[tile]);
       this.phaser.load.image(this.tileArray[tile], 'assets/base_pack/Spritesheet/'+this.tileArray[tile]);
     }
 
+    //Load hints
+    this.hint = "stoneRing.png";
+    this.phaser.load.image("stoneRing.png", 'assets/base_pack/Spritesheet/stoneRing.png');
 
+  }
 
-
-  },
-  create: function() {
+  create(){
     // setup game environment
     // scale, input etc..
 
     this.phaser.stage.backgroundColor = '#ca9a6c';
     console.log("game is created");
 
-    this.phaser.world.setBounds(0, 0, 680, 1400);
+    this.phaser.world.setBounds(0, 0, 2000, 4000);
 
-  /*
-    this.phaser.add.sprite(65/2, 89+72, 'tileWater.png');
-    this.phaser.add.sprite(65+65/2, 89+72, 'tileLava_full.png');
-    this.phaser.add.sprite(0, 89, 'tileMagic.png');
-    this.phaser.add.sprite(65, 89, 'tileStone.png');
-*/
 
+    //Create background images
     for (var topLeft = 0; topLeft < 30; topLeft = topLeft +1){
 
       var offSet = topLeft*107;
@@ -71,11 +73,32 @@ Game.prototype = {
         this.phaser.add.sprite(66*rightOff-65/2, offSet+53, this.tileArray[Math.floor(Math.random()*this.tileArray.length)]);
       }
     }
-  },
 
-  update: function(){
+    //Add a hind
+    this.phaser.add.sprite(0, 0, "stoneRing.png");
 
+    this.cursors = this.phaser.input.keyboard.createCursorKeys();
   }
-};
+
+  update(){
+    if (this.cursors.up.isDown)
+    {
+      this.phaser.camera.y -= 4;
+    }
+    else if (this.cursors.down.isDown)
+    {
+      this.phaser.camera.y += 4;
+    }
+
+    if (this.cursors.left.isDown)
+    {
+      this.phaser.camera.x -= 4;
+    }
+    else if (this.cursors.right.isDown)
+    {
+      this.phaser.camera.x += 4;
+    }
+  }
+}
 
 
