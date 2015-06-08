992b0a0e4c933960ca5cd5cef6fa7dbec27294e9
BrandiATMuhkuh <juergen@tagaprice.org>
2015-06-08 17:28:01 +1200

    i created a python rest server to communicate with nao via http

--- NAOResearch/webServerToNaoSay.py
+++ NAOResearch/webServerToNaoSay.py
@@ -3,14 +3,14 @@ from naoqi import ALProxy
 tts = ALProxy("ALTextToSpeech", '10.32.48.160', 9559)
 
 class HelloWorld(object):
-    
+
     @cherrypy.expose
     def index(self, a='', b='empty', say="Hello"):
 	test = " juhu "
         print "Hello World!3 "+a+say
 	tts.say(str(say))
         return "Hello World!3 "+a+say
-    
+
     index.exposed = True
 cherrypy.server.socket_host = '0.0.0.0'
 cherrypy.quickstart(HelloWorld())
