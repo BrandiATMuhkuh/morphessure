bd64a232f13f9bca59b29c49f78c7764d6fc3fd4
jbr134 <jbr134@Mark-Billinghursts-MacBook-Air.local>
2015-07-11 19:15:41 +1200

    added move hand and move head to naobehavior

--- naoBehavor/exampleClient.py
+++ naoBehavor/exampleClient.py
@@ -1,10 +1,10 @@
-import socket 
+import socket
 
-host = 'cookie.local' 
-port = 50016 
-size = 1024 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-s.connect((host,port)) 
-s.send('say=hello how are you?&volume=50')
-#data = s.recv(size) 
-s.close() 
+host = 'cookie'
+port = 50016
+size = 1024
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host,port))
+s.send('say=Your turn&volume=50&moveHand=true&lookUp=false')
+#data = s.recv(size)
+s.close()
