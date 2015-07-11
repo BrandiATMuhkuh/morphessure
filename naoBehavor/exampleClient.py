6f0392a6ec56ab7658262c8fcc87e051793dac4d
BrandiATMuhkuh <juergen@tagaprice.org>
2015-07-11 16:58:24 +1200

    hintlinst for tutorial is done

--- naoBehavor/exampleClient.py
+++ naoBehavor/exampleClient.py
@@ -1,10 +1,10 @@
-import socket 
+import socket
 
-host = 'cookie.local' 
-port = 50016 
-size = 1024 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-s.connect((host,port)) 
+host = 'cookie.mshome.net' 
+port = 50016
+size = 1024
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host,port))
 s.send('say=hello how are you?&volume=50')
-#data = s.recv(size) 
-s.close() 
+#data = s.recv(size)
+s.close()
