d7042400619e7c12503b13b22d6fe6f4d120cae6
BrandiATMuhkuh <juergen@tagaprice.org>
2015-07-16 18:31:12 +1200

    start script for RA waits now for inputs

--- GameServer/startServer.sh
+++ GameServer/startServer.sh
@@ -1,2 +1,15 @@
-trap "kill 0" SIGINT
-(python naoServer/index.py) & (grunt nodemon) & (grunt serve) 
+#!/bin/bash
+
+read -e -p "Enter condition number: " -i "" CONDITION
+#echo $CONDITION
+
+read -e -p "Enter participant id: " -i "" PARTICIPANTID
+#echo $PARTICIPANTID
+
+read -e -p "Enter robot address: " -i "" ROBOTADDRESS
+#echo $ROBOTADDRESS
+
+echo "iosj server/index.js --condition $CONDITION --pId $PARTICIPANTID --robotAddress $ROBOTADDRESS"
+
+#trap "kill 0" SIGINT
+#(python naoServer/index.py) & (grunt nodemon) & (grunt serve)
