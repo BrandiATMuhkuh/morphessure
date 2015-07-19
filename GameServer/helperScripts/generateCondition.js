7234d7548b7726a3f0c6dcd752779dffe7f870ee
BrandiATMuhkuh <juergen@tagaprice.org>
2015-07-19 21:34:54 +1200

    Created main map generator that takes manipulations symbols into account

---
+++ GameServer/helperScripts/generateCondition.js
@@ -0,0 +1,213 @@
+require('./../app/assets/Assets.js');
+var fs = require('fs');
+var x = 10; //The maximul number of columns
+var y = 80; //The number of rows
+var traps = Assets.traps; // all traps
+var field2 = new Array(y);
+
+var configObject = {
+   playerNames: [{name: "player1", map:[], gap:0, field : new Array(y)}, {name: "player2", map:[], gap:0, field : new Array(y)}],
+   symbolPatterns: [
+      {name: "belly", pos:0, gap: 10, pattern: [{name:"player1", pos:null}, {name:"player1", pos:null}, {name:"player2", pos:null}, {name:"player1", pos:null}]},
+      {name: "car", pos:0, gap: 10, pattern: [{name:"player1", pos:null}, {name:"player1", pos:null}, {name:"player2", pos:null}, {name:"player1", pos:null}]},
+      {name: "knife", pos:0, gap: 10, pattern: [{name:"player1", pos:null}, {name:"player1", pos:null}, {name:"player2", pos:null}, {name:"player1", pos:null}]},
+      {name: "snack", pos:0, gap: 10, pattern: [{name:"player2", pos:null}, {name:"player2", pos:null}, {name:"player2", pos:null}, {name:"player2", pos:null}]}
+   ],
+   gap: 3,
+   maxMapLength: 60
+};
+
+
+
+//console.log(traps);
+genManipulationMap();
+for(var i = 0; i < configObject.playerNames.length; i++){
+   var fi = configObject.playerNames[i].field;
+   generateEmptyField(fi);
+   fillMapWithPreTrap(fi, configObject.playerNames[i]);
+   fillField(fi);
+   //console.log(formatToCondition(fi));
+}
+console.log(formatToCondition(configObject.playerNames[1].field));
+
+
+function genManipulationMap(){
+   for(var i=0; i<configObject.maxMapLength*configObject.playerNames.length; i = i + 1){
+      var mod = i % configObject.playerNames.length; //select player
+      var mastPlayer = getPlayer(configObject.playerNames[mod].name);
+
+      var s = getSymbol(mastPlayer.name);
+
+      if(s === null){
+         mastPlayer.map.push(null);
+      }else{
+         s.player.pos = mastPlayer.map.length;
+         s.symbol.pos = s.symbol.pos + 1;
+         mastPlayer.map.push(s.symbol.name);
+      }
+
+   }
+}
+
+function getSymbol(playerName){
+   var symbol = randomSymbol();
+   var masterPlayer = getPlayer(playerName);
+   if(symbol.pos < symbol.pattern.length){
+      var player = symbol.pattern[symbol.pos];
+      if(player.name === playerName){
+         return {symbol: symbol, player: player};
+      }
+   }
+
+
+   return null;
+}
+
+function randomSymbol(){
+  return configObject.symbolPatterns[Math.floor(Math.random()*configObject.symbolPatterns.length)];
+}
+
+function getPlayer(name){
+   for(var a in configObject.playerNames){
+      if(configObject.playerNames[a].name === name){
+         return configObject.playerNames[a];
+      }
+   }
+
+   return null;
+}
+
+function generateEmptyField(field){
+
+   for(var i = 0; i < field.length; i = i + 1){
+      field[i] = new Array(x);
+
+      for(var k = 0; k < field[i].length; k++){
+         field[i][k] = -1;
+      }
+   }
+}
+
+function randomElementNotInList(){
+   var wordArr = [];
+   for(var a in configObject.symbolPatterns){
+      wordArr.push(configObject.symbolPatterns[a].name);
+   }
+
+   var v = randomTrapElement();
+   while(wordArr.indexOf(v) > -1){
+      v = randomTrapElement();
+   }
+
+   return v;
+
+   //console.log(v);
+}
+
+function randomTrapElement(){
+  return traps[Math.floor(Math.random()*traps.length)];
+}
+
+function fillMapWithPreTrap(field, playerName){
+   var forward = false;
+   var n = 0
+   for(var i=1;i < y;i = i + 1){
+
+      if(i%2 == 1){
+         forward = !forward;
+         if(forward){
+            field[i][1] = ""+n;
+            n++;
+         }else{
+            field[i][8] = ""+n;
+            n++;
+         }
+      }else{
+         if(forward){
+            for(var b=1; b<9;b=b+1){
+               field[i][b] = ""+n;
+               n++;
+            }
+         }else{
+            for(var b=1; b<9;b=b+1){
+               field[i][9-b] = ""+n;
+               n++;
+            }
+         }
+      }
+   }
+
+   //add read VALUES
+   for(var i = 0; i < field.length; i = i + 1){
+     for(var k = 0; k < field[i].length; k = k + 1){
+        if(field[i][k]){
+           if(playerName.map[parseInt(field[i][k])]){
+             field[i][k] = playerName.map[parseInt(field[i][k])];
+          }else{
+             field[i][k] = -1;
+          }
+        }
+     }
+  }
+}
+
+function fillField(field){
+  for(var i = 0; i < field.length; i = i + 1){
+    for(var k = 0; k < field[i].length; k = k + 1){
+      //console.log(field[i][k]);
+      if(field[i][k] === -1){
+         var tElem = randomElementNotInList();
+         //console.log("should be empty", field[i][k]);
+         while(!isAlone(tElem, i, k, field)){
+           tElem = randomElementNotInList();
+         }
+         field[i][k] = tElem;
+      }
+    }
+  }
+}
+
+function isAlone(word, x, y, field){
+  //console.log(word, x, y);
+
+  if(field[x-1] !== undefined && field[x-1][y-1] !== undefined && field[x-1][y-1] === word){
+      return false;
+  }
+
+  if(field[x-1] !== undefined && field[x-1][y] !== undefined && field[x-1][y] === word){
+      return false;
+  }
+
+  if(field[x] !== undefined && field[x][y-1] !== undefined && field[x][y-1] === word){
+      return false;
+  }
+
+  if(field[x] !== undefined && field[x][y+1] !== undefined && field[x][y+1] === word){
+      return false;
+  }
+
+  if(field[x+1] !== undefined && field[x+1][y-1] !== undefined && field[x+1][y-1] === word){
+      return false;
+  }
+
+  if(field[x+1] !== undefined && field[x+1][y] !== undefined && field[x+1][y] === word){
+      return false;
+  }
+
+  return true;
+}
+
+function formatToCondition(field){
+  var formArray = new Array();
+  for(var i = 0; i < field.length; i = i + 1){
+    for(var k = 0; k < field[i].length; k = k + 1){
+      console.log(i,k,field[i][k]);
+      formArray.push({
+        position: [i,k],
+        name: field[i][k]
+      });
+    }
+  }
+
+  return formArray;
+}
