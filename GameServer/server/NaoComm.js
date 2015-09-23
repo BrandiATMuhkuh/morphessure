68d1c84190cd966f6d86db8b9c9ae64e0e2267a7
BrandiATMuhkuh <juergen@tagaprice.org>
2015-09-23 09:26:06 +0100

    nao can now talk again

--- GameServer/server/NaoComm.js
+++ GameServer/server/NaoComm.js
@@ -85,7 +85,7 @@ module.exports = class NaoComm {
   send(){
 
     console.log("nao send", this.isClosed, this.sendCommands.length);
-    /*
+    
     if(this.isClosed && this.sendCommands.length > 0){
       this.isClosed = false;
       var client = new net.Socket();
@@ -104,7 +104,7 @@ module.exports = class NaoComm {
         console.log('Connection closed');
         this.send();
       }.bind(this));
-    }*/
+    }
 
   }
 
