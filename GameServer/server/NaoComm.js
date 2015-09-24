066304b11e16976e530b3c83c4d896a3a6284788
BrandiATMuhkuh <juergen@tagaprice.org>
2015-09-24 01:10:12 +0100

    rolled back naoComm to a working one

--- GameServer/server/NaoComm.js
+++ GameServer/server/NaoComm.js
@@ -1,124 +1,118 @@
 /**
- * This class does all the communication between the server and NAO
- */
+* This class does all the communication between the server and NAO
+*/
 'use strict';
 var request = require('request');
 var net = require('net');
 module.exports = class NaoComm {
 
-    constructor(address, port, debugFlag) {
-        this.address = (typeof address === 'undefined') ? "localhost" : address;
-        this.port = (typeof port === 'undefined') ? 50016 : port;
-        this.sendCommand = 'say=' + "Nothing to say" + '&';
+  constructor(address, port){
+    this.address = (typeof address === 'undefined')? "localhost":address;
+    this.port = (typeof port === 'undefined')? 50016:port;
+    this.sendCommand = 'say=' + "Nothing to say" + '&';
+    this.isClosed = true;
+    this.sendCommands = [];
+  }
+
+  /**
+   * Set what robot says
+   * @param text
+   */
+  say(text){
+    this.sendCommand = this.sendCommand + 'say=' + text + '&'
+    console.log("Nao should say:", text);
+    return this;
+  }
+
+  /**
+   * Tell robot to look up or down
+   * @param up true/false
+   */
+  lookUp(up){
+    this.sendCommand = this.sendCommand + 'lookUp=' + up + '&';
+    console.log("Robot look:", up);
+    return this;
+  }
+
+  /**
+   * set the volume of the robot
+   * @param vol 0-100
+   */
+  volume(vol){
+    this.sendCommand = this.sendCommand + 'volume=' + vol + '&';
+    console.log("Robot volume:", vol);
+    return this;
+  }
+
+
+  /**
+   * will move robots hand randomly
+   */
+  moveHand(){
+    this.sendCommand = this.sendCommand + 'moveHand=' + true + '&';
+    console.log("Robot moves hand");
+    return this;
+  }
+
+  /**
+   * Robot initialices sit position
+   */
+  initSit(){
+    this.sendCommand = this.sendCommand + 'initSit=' + true + '&';
+    console.log("Robot initSit");
+    return this;
+  }
+
+  /**
+  * Robot start the trust me behavior
+  */
+  trustgame(){
+    this.sendCommand = this.sendCommand + 'trustgame=' + true + '&';
+    console.log("Robot trustgame");
+    return this;
+  }
+
+  start(){
+    this.sendCommand = "";
+    return this;
+  }
+
+  finish(){
+    this.sendCommands.unshift(""+this.sendCommand);
+    return this;
+  }
+
+  send(){
+
+    console.log("nao send", this.isClosed, this.sendCommands.length);
+    
+    if(this.isClosed && this.sendCommands.length > 0){
+      this.isClosed = false;
+      var client = new net.Socket();
+      client.on('error', function(e){
+        console.error("I could not connect to NAO. I guess the behavior is not running or NAO is not durned on!");
+      });
+
+      client.connect(this.port, this.address, function () {
+        console.log('Connected');
+
+        client.write(this.sendCommands.pop());
+      }.bind(this));
+
+      client.on('close', function () {
         this.isClosed = true;
-        this.sendCommands = [];
-        this.debugFlag = debugFlag;
-    }
-
-    /**
-     * Set what robot says
-     * @param text
-     */
-    say(text) {
-        this.sendCommand = this.sendCommand + 'say=' + text + '&'
-        console.log("Nao should say:", text);
-        return this;
-    }
-
-    /**
-     * Tell robot to look up or down
-     * @param up true/false
-     */
-    lookUp(up) {
-        this.sendCommand = this.sendCommand + 'lookUp=' + up + '&';
-        console.log("Robot look:", up);
-        return this;
-    }
-
-    /**
-     * set the volume of the robot
-     * @param vol 0-100
-     */
-    volume(vol) {
-        this.sendCommand = this.sendCommand + 'volume=' + vol + '&';
-        console.log("Robot volume:", vol);
-        return this;
-    }
-
-
-    /**
-     * will move robots hand randomly
-     */
-    moveHand() {
-        this.sendCommand = this.sendCommand + 'moveHand=' + true + '&';
-        console.log("Robot moves hand");
-        return this;
-    }
-
-    /**
-     * Robot initialices sit position
-     */
-    initSit() {
-        this.sendCommand = this.sendCommand + 'initSit=' + true + '&';
-        console.log("Robot initSit");
-        return this;
-    }
-
-    /**
-     * Robot start the trust me behavior
-     */
-    trustgame() {
-        this.sendCommand = this.sendCommand + 'trustgame=' + true + '&';
-        console.log("Robot trustgame");
-        return this;
-    }
-
-    start() {
-        this.sendCommand = "";
-        return this;
-    }
-
-    finish() {
-        this.sendCommands.unshift("" + this.sendCommand);
-        return this;
-    }
-
-    send() {
-
-        console.log("nao send", this.isClosed, this.sendCommands.length);
-
-        if (this.debugFlag === false) {
-            if (this.isClosed && this.sendCommands.length > 0) {
-                this.isClosed = false;
-                var client = new net.Socket();
-                client.on('error', function(e) {
-                    console.error("I could not connect to NAO. I guess the behavior is not running or NAO is not durned on!");
-                });
-
-                client.connect(this.port, this.address, function() {
-                    console.log('Connected');
-
-                    client.write(this.sendCommands.pop());
-                }.bind(this));
-
-                client.on('close', function() {
-                    this.isClosed = true;
-                    console.log('Connection closed');
-                    this.send();
-                }.bind(this));
-            }
-
-        }
-
-
-
-    }
-
-    /**
-     * Repeats the last thing nao said
-     */
-    repeat() {
-        this.sendCommands.unshift("" + this.sendCommand);
+        console.log('Connection closed');
         this.send();
+      }.bind(this));
     }
+
+  }
+
+  /**
+  * Repeats the last thing nao said
+  */
+  repeat(){
+    this.sendCommands.unshift(""+this.sendCommand);
+    this.send();
+  }
 }
