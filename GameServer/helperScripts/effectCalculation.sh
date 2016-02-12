086925b7797f567b65e422bc17e9167a7dd6484f
BrandiATMuhkuh <juergen@tagaprice.org>
2016-02-12 13:48:35 +1300

    changed location of database

--- GameServer/helperScripts/effectCalculation.sh
+++ GameServer/helperScripts/effectCalculation.sh
@@ -1,9 +1,16 @@
+#Description
+#This script calucates the effect the experiment had on the words which are used. Not the valance shift!
+#autoput is on stdout. Use for example 'effectCalulation.sh > shift.csv' to save the output
+
+#Install Requirements
+#sudo apt-get install sqlite3 libsqlite3-dev
+
 #This file will download the analysis data from a given sqlite database
-DATABASE='/home/brandiatmuhkuh/pCloudDrive/Shared/morphessorSingleFiles/logging.sqlite'
+DATABASE='/home/jbr134/pCloudDrive/morphessorSingleFiles/logging.sqlite'
 showeffect=0 #set this to 1 if you want the overall effect
 
 #Get all user with pId from database
-TS=`sqlite3 $DATABASE "select DISTINCT pId, condition from log where timestamp > 1443056621939 AND pId NOT GLOB '*[A-Za-z]*'"`
+TS=`sqlite3 $DATABASE "select DISTINCT pId, condition from log where timestamp > 1443056621939 AND pId NOT GLOB '*[A-Za-z]*' and pId NOT GLOB '111*'"`
 
 #header on Off
 header='pId,cId,preWord,robotWord,postWord,symbolName,strategy,effect'
@@ -22,10 +29,10 @@ IFS='|' read -ra ADDR <<< "$T"
 #echo ${ADDR[0]}
 #echo ${ADDR[1]}
 
-STATEMENT="select  '${ADDR[0]}' as pId, '${ADDR[1]}' as cId, preWord, quote(robotWord), word as postWord, post.symbolName as symbolName, change as strategy, 
+STATEMENT="select  '${ADDR[0]}' as pId, '${ADDR[1]}' as cId, preWord, quote(robotWord), word as postWord, post.symbolName as symbolName, change as strategy,
 	CASE WHEN preWord != word and change = 'NA'
-	THEN 'NRCHANGE' 
-	ELSE 
+	THEN 'NRCHANGE'
+	ELSE
 		CASE WHEN preWord = word and change = 'NA'
 		THEN 'NRMATCH'
 		ELSE
@@ -37,22 +44,22 @@ STATEMENT="select  '${ADDR[0]}' as pId, '${ADDR[1]}' as cId, preWord, quote(robo
 	END as effect
 
  from (
-select pre.word as preWord, robot.word as robotWord, pre.symbolName, correct , 
-	CASE WHEN pre.word = robot.word 
+select pre.word as preWord, robot.word as robotWord, pre.symbolName, correct ,
+	CASE WHEN pre.word = robot.word
 	THEN 'MATCH'
-	ELSE 
+	ELSE
 		CASE
-		WHEN robot.word is null 
+		WHEN robot.word is null
 		THEN 'NA'
 		ELSE 'CHANGE'
 		END
 	END AS change
 from (
 select word, symbolName, correct from log where condition = '${ADDR[1]}' and pId = '${ADDR[0]}' and level = 'singlePlayer' and type = 'LogPlayerSaid' ORDER by ISOTime
-) as pre 
-LEFT JOIN (select DISTINCT word, symbolName from log where condition = '${ADDR[1]}' and pId = '${ADDR[0]}'  and transmitter = 'player2' and level = 'multiPlayer' and type = 'LogPlayerShouldSay' ORDER by ISOTime) as robot 
-on pre.symbolName = robot.symbolName) as preRobot 
-LEFT JOIN (select word, symbolName, correct from log where condition = '${ADDR[1]}' and pId = '${ADDR[0]}' and level = 'secSinglePlayer' and type = 'LogPlayerSaid' ORDER by ISOTime) as post 
+) as pre
+LEFT JOIN (select DISTINCT word, symbolName from log where condition = '${ADDR[1]}' and pId = '${ADDR[0]}'  and transmitter = 'player2' and level = 'multiPlayer' and type = 'LogPlayerShouldSay' ORDER by ISOTime) as robot
+on pre.symbolName = robot.symbolName) as preRobot
+LEFT JOIN (select word, symbolName, correct from log where condition = '${ADDR[1]}' and pId = '${ADDR[0]}' and level = 'secSinglePlayer' and type = 'LogPlayerSaid' ORDER by ISOTime) as post
 on preRobot.symbolName = post.symbolName"
 
 #echo $STATEMENT
@@ -65,8 +72,8 @@ if [ $showeffect -eq 1 ]
 
 select *,
 	CASE WHEN preWord != word and change = 'NA'
-	THEN 'NRCHANGE' 
-	ELSE 
+	THEN 'NRCHANGE'
+	ELSE
 		CASE WHEN preWord = word and change = 'NA'
 		THEN 'NRMATCH'
 		ELSE
@@ -78,23 +85,23 @@ select *,
 	END as effect
 
  from (
-select pre.word as preWord, robot.word as robotWord, pre.symbolName, correct , 
-	CASE WHEN pre.word = robot.word 
+select pre.word as preWord, robot.word as robotWord, pre.symbolName, correct ,
+	CASE WHEN pre.word = robot.word
 	THEN 'MATCH'
-	ELSE 
+	ELSE
 		CASE
-		WHEN robot.word is null 
+		WHEN robot.word is null
 		THEN 'NA'
 		ELSE 'CHANGE'
 		END
 	END AS change
 from (
 select word, symbolName, correct from log where condition = '${ADDR[1]}' and pId = '${ADDR[0]}' and level = 'singlePlayer' and type = 'LogPlayerSaid' ORDER by ISOTime
-) as pre 
-LEFT JOIN (select DISTINCT word, symbolName from log where condition = '${ADDR[1]}' and pId = '${ADDR[0]}'  and transmitter = 'player2' and level = 'multiPlayer' and type = 'LogPlayerShouldSay' ORDER by ISOTime) as robot 
-on pre.symbolName = robot.symbolName) as preRobot 
-LEFT JOIN (select word, symbolName, correct from log where condition = '${ADDR[1]}' and pId = '${ADDR[0]}' and level = 'secSinglePlayer' and type = 'LogPlayerSaid' ORDER by ISOTime) as post 
-on preRobot.symbolName = post.symbolName 
+) as pre
+LEFT JOIN (select DISTINCT word, symbolName from log where condition = '${ADDR[1]}' and pId = '${ADDR[0]}'  and transmitter = 'player2' and level = 'multiPlayer' and type = 'LogPlayerShouldSay' ORDER by ISOTime) as robot
+on pre.symbolName = robot.symbolName) as preRobot
+LEFT JOIN (select word, symbolName, correct from log where condition = '${ADDR[1]}' and pId = '${ADDR[0]}' and level = 'secSinglePlayer' and type = 'LogPlayerSaid' ORDER by ISOTime) as post
+on preRobot.symbolName = post.symbolName
 ) group by effect"
 fi
 
