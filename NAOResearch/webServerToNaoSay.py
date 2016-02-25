d6ae5f254920d1333d1bd3d8845321610b291f03
BrandiATMuhkuh <juergen@tagaprice.org>
2016-02-25 15:35:57 +1300

    modified the nao webserver to adjust the volume

--- NAOResearch/webServerToNaoSay.py
+++ NAOResearch/webServerToNaoSay.py
@@ -1,15 +1,21 @@
 import cherrypy
 from naoqi import ALProxy
-tts = ALProxy("ALTextToSpeech", '10.32.48.160', 9559)
+tts = ALProxy("ALTextToSpeech", 'dontusethisrobot.canterbury.ac.nz', 9559)
 
 class HelloWorld(object):
 
     @cherrypy.expose
-    def index(self, a='', b='empty', say="Hello"):
-	test = " juhu "
-        print "Hello World!3 "+a+say
-	tts.say(str(say))
-        return "Hello World!3 "+a+say
+    def index(self, volume=None, say=None):
+    	#test = " juhu "
+        #print "Hello World!3 "+a+say
+        if volume is not None:
+        	print ("volume: ", volume)
+        	tts.setVolume(float(volume))
+
+        if say is not None:
+        	print ("say: ", say)
+        	tts.say(str(say))
+        return "done";
 
     index.exposed = True
 cherrypy.server.socket_host = '0.0.0.0'
